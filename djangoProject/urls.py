f97acd164ff5d68f6f1a5aa9daeb38134e33f872
Erdemozer01 <20219248004@cumhuriyet.edu.tr>
2022-07-21 02:59:11 +0300

    değişiklikler

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
                   path('accounts/', include('accounts.urls')),
                   path('', include('blog.urls')),
                   path('post/', include('post.urls')),
+                  path('logout/', LogoutView.as_view(), name="logout"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
