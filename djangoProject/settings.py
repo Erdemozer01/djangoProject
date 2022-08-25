b0f11b0c61534a1b29b44045ba02bee03ddc7243
Erdemozer01 <20219248004@cumhuriyet.edu.tr>
2022-08-25 11:08:26 +0300

    değişiklikler

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
