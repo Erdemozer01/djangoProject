7a6b503f7f0ee53ad322f0f789dfff3f11478111
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-08 22:25:55 +0300

    saved

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MUSCLE_DIR = BASE_DIR / 'bioinformatic/muscle5.1.linux_intel64'
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
