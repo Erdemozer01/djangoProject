4837d46aad4e423c5f2c4125fba3e86da38041e4
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-09 11:46:09 +0300

    saved

--- bioinformatic/views/trees.py
+++ bioinformatic/views/trees.py
@@ -89,7 +89,6 @@ def FastaCreateTreesView(request):
 
                 method = form.cleaned_data['method']
 
-
                 if not ".fasta" in file:
                     msg = "Hatalı Dosya uzantısı, Lütfen .fasta uzantılı dosyası seçiniz"
                     url = reverse("bioinformatic:filogenetik_agac_fasta")
