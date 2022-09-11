11ad0ec1f3a68d43518392f69398257f58566bbd
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-11 20:37:35 +0300

    saved

--- bioinformatic/views/trees.py
+++ bioinformatic/views/trees.py
@@ -70,8 +70,6 @@ def MuscleTreesView(request):
 
                 muscle_cline = MuscleCommandline(muscle_exe, input=input_file, out=output_file)
 
-                muscle_result = subprocess.check_output([muscle_exe, "-in", input_file, "-out", output_file])
-
                 AlignIO.convert(output_file, "fasta", align_file, "clustal")
 
                 alignment = AlignIO.read(align_file, "clustal")
