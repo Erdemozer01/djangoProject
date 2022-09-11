370b490b58f2973596eea9650c3a170d4f74f02c
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-12 00:50:45 +0300

    saved

--- bioinformatic/views/trees.py
+++ bioinformatic/views/trees.py
@@ -68,7 +68,9 @@ def MuscleTreesView(request):
                 elif sys.platform.startswith('linux'):
                     muscle_exe = os.path.join(BASE_DIR, 'bioinformatic', 'apps', 'muscle3.8.425_i86linux32')
 
-                muscle_result = subprocess.check_output([muscle_exe, "-in", input_file, "-out", output_file][0])
+                muscle_result = subprocess.check_output([muscle_exe, "-in", input_file, "-out", output_file])
+
+                print(muscle_result)
 
                 AlignIO.convert(output_file, "fasta", align_file, "clustal")
 
