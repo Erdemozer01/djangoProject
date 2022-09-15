6bfedc1acc44d1aa2a3144bc1dd6892dad090de5
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-15 21:28:08 +0300

    saved

--- bioinformatic/views/alignments.py
+++ bioinformatic/views/alignments.py
@@ -168,7 +168,7 @@ def MultipleSeqAlignment(request):
 
                     input_file = os.path.join(BASE_DIR, 'bioinformatic', 'files',
                                               '{}'.format(form.cleaned_data['file']))
-                    output_file = os.path.join(BASE_DIR, 'bioinformatic', 'files', 'aligned.aln')
+                    output_file = os.path.join(BASE_DIR, 'bioinformatic', 'files', 'aligned.fasta')
                     align_file = os.path.join(BASE_DIR, 'bioinformatic', 'files', "align.aln")
                     tree_file = os.path.join(BASE_DIR, 'bioinformatic', 'files', 'tree.xml')
 
