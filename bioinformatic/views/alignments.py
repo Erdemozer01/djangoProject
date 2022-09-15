374a56dfb726b1f055197df217e6c3e94ad569d2
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-15 14:52:43 +0300

    saved

--- bioinformatic/views/alignments.py
+++ bioinformatic/views/alignments.py
@@ -189,7 +189,7 @@ def MultipleSeqAlignment(request):
                     stdout, stderr = clustalw_cline()
 
                     AlignIO.convert(output_file, 'fasta', align_file, 'clustal')
-                    alignment = AlignIO.read(output_file, "clustal")
+                    alignment = AlignIO.read(align_file, "clustal")
                     calculator = DistanceCalculator('identity')
 
                     constructor = DistanceTreeConstructor(calculator, method=algoritma)
