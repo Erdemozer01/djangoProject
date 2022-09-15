bdda70749f27197f1d414bb582cfeb34595ecdb9
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-15 21:38:51 +0300

    saved

--- bioinformatic/views/alignments.py
+++ bioinformatic/views/alignments.py
@@ -190,10 +190,12 @@ def MultipleSeqAlignment(request):
                         assert os.path.isfile(os.path.join(BASE_DIR, "bioinformatic", "apps", "clustalw2"))
                         clustalw_cline()
                     elif sys.platform.startswith('linux'):
-                        clustal_result = subprocess.Popen(str(clustalw_cline), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=(sys.platform!="win32"))
+                        clustal_result = subprocess.Popen(str(clustalw_cline), stdin=subprocess.PIPE,
+                                                          stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                                          universal_newlines=True, shell=(sys.platform != "win32"))
 
                     AlignIO.convert(output_file, 'fasta', align_file, 'clustal')
-                    alignment = AlignIO.read(align_file, "clustal")
+                    alignment = AlignIO.read(output_file, "clustal")
 
                     calculator = DistanceCalculator('identity')
 
