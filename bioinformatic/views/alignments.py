b6a71e6ad5a48836edd2681371131e699ae5bc2b
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-16 19:47:59 +0300

    saved

--- bioinformatic/views/alignments.py
+++ bioinformatic/views/alignments.py
@@ -190,7 +190,7 @@ def MultipleSeqAlignment(request):
                     assert os.path.isfile(clustalw2_exe), "Clustal W executable missing"
                     stdout, stderr = clustalw_cline()
 
-                    alignment = next(AlignIO.read(output_file, "clustal"))
+                    alignment = AlignIO.read(open(output_file, 'r'), "clustal")
 
                     calculator = DistanceCalculator('identity')
                     constructor = DistanceTreeConstructor(calculator, method=algoritma)
