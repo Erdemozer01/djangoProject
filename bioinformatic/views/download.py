2c7887980c0a9fc136737c856e923607d505b273
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-17 16:18:41 +0300

    saved

--- bioinformatic/views/download.py
+++ bioinformatic/views/download.py
@@ -257,6 +257,38 @@ def clustal_stats_download(request):
     finally:
         os.remove(filepath)
 
+
+def clustal_scores_download(request):
+    try:
+        # Define Django project base directory
+        BASE_DIR = Path(__file__).resolve().parent.parent.parent
+        # Define text file name
+        filename = "scores.txt"
+        # Define the full file path
+        filepath = os.path.join(BASE_DIR, "bioinformatic", 'files', 'scrores.txt')
+        # Open the file for reading content
+        path = open(filepath, 'rb').read()
+        # Set the mime type
+        mime_type, _ = mimetypes.guess_type(filepath)
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        # Return the response value
+    except FileNotFoundError:
+        msg = "İndirmeye çalıştığınız dosya bulunamadı"
+        url = reverse("bioinformatic:multiplesequence_alignments")
+        return render(request, 'bioinformatic/fasta/notfound.html',
+                      {"msg": msg, 'url': url})
+    try:
+        return response
+    except FileNotFoundError:
+        msg = "İndirmeye çalıştığınız dosya bulunamadı"
+        return render(request, 'bioinformatic/fasta/notfound.html',
+                      {"msg": msg})
+    finally:
+        os.remove(filepath)
+
 def global_alignments_download(request):
     # Define Django project base directory
     BASE_DIR = Path(__file__).resolve().parent.parent
