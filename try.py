4683882425d9bc5d5aa8367bbebf186628973aef
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-09-27 01:28:08 +0300

    saved

--- try.py
+++ try.py
@@ -9,8 +9,9 @@ cml = codeml.Codeml()
 cml.alignment = os.path.join(BASE_DIR, "bioinformatic", "files", "aligment.fasta")
 cml.tree = os.path.join(BASE_DIR, "bioinformatic", "files", "tree.xml")
 cml.ctl_file = os.path.join(BASE_DIR, "bioinformatic", "files", "codeml.ctl")
-cml.out_file = os.path.join(BASE_DIR, "bioinformatic", "files", "result_basemlL.txt")
+cml.out_file = os.path.join(BASE_DIR, "bioinformatic", "files", "result_codeml.txt")
 cml.working_dir = os.path.join(BASE_DIR, "bioinformatic", "files")
-cml_exe = os.path.join(BASE_DIR, "bioinformatic", "apps", "paml4.9j/bin/codeml.exe")
+cml_exe = os.path.join(BASE_DIR, "bioinformatic", "apps", "paml4.9j", "bin", "codeml.exe")
+
 
 results = cml.run(verbose=True, command=cml_exe)
