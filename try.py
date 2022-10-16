04da6dc5a8ee4054f80515982a84d7ceb0a5e7b6
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-10-16 10:47:58 +0300

    saved

--- try.py
+++ try.py
@@ -46,7 +46,7 @@ print("\n")
 print("normalize".title())
 print(motif.counts.normalize(pseudocounts=0.4))
 print("\n")
-pwm = motif.counts.normalize(pseudocounts={"A":0.6, "C": 0.4, "G": 0.4, "T": 0.6})
+pwm = motif.counts.normalize(pseudocounts={"A": 0.6, "C": 0.4, "G": 0.4, "T": 0.6})
 print(pwm)
 print("pwd:")
 print(pwm.consensus)
@@ -73,3 +73,76 @@ print(jaspar_motif.pseudocounts)
 from numpy import array
 
 from Bio.Cluster import distancematrix
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from Bio.Graphics import GenomeDiagram
+from Bio import SeqIO
+
+records = SeqIO.parse(os.path.join(BASE_DIR, "bioinformatic", "files", "ls_orchid.gbk.txt"), "genbank")
+
+name = []
+
+for record in records:
+    name.append(record)
+
+record = name[0]
+
+gd_diagram = GenomeDiagram.Diagram(record.description)
+gd_track_for_features = gd_diagram.new_track(1, name="Annotated Features")
+gd_feature_set = gd_track_for_features.new_set()
+
+for feature in record.features:
+    if feature.type != "gene":
+        # Exclude this feature
+        continue
+    if len(gd_feature_set) % 2 == 0:
+        color = colors.blue
+    else:
+        color = colors.lightblue
+    gd_feature_set.add_feature(feature.feature, color=color, label=True)
+
+gd_diagram.draw(
+    format="linear",
+    orientation="landscape",
+    pagesize="A4",
+    fragments=4,
+    start=0,
+    end=len(record),
+)
+
+gd_diagram.write("plasmid_linear.pdf", "PDF")
+
+# Import Python modules
+from Bio import GenBank
+from reportlab.lib import colors
+from GenomeDiagram import GDDiagram, GDUtilities
+
+# Load genome annotations from GenBank file
+parser = GenBank.FeatureParser()
+fhandle = open(file_path, "r")
+genbank_entry = parser.parse(fhandle)
+fhandle.close()
+# Draw linear diagram of CDS features, with GC content graph
+gdd = GDDiagram(file_path)
+gdt1 = gdd.new_track(4, greytra
+CDS’,
+scale_fontsize¼3, greytrack_fontsize¼3)
+gdt2 ¼ gdd.new_track(6, greytrack¼1, name¼‘Cv
+GC
+content’,
+scale_fontsize¼3, greytrack_fontsize¼3, height¼2)
+gdfs ¼ gdt1.new_set(‘feature’)
+gdgs ¼ gdt2.new_set(‘graph’)
+graphdata1 ¼ GDUtilities.gc_content(genbank_entry.seq, 1000)
+graph1 ¼ gdgs.new_graph(graphdata1, ‘GC
+content’, style¼‘line’,
+colour¼colors.blue, altcolour¼colors.purple)
+graph1.linewidth¼1
+for feature in genbank_entry.features:
+    if feature.type ¼¼ ‘CDS’:
+    gdfs.add_feature(feature, colour¼colors.red)
+gdd.draw(format¼‘linear’, orientation¼‘landscape’, tracklines¼0,
+                                                              pagesize¼‘A6’, fragments¼10, circular¼1)
+# Write image as a PNG raster file, and as a PDF vector image
+gdd.write(‘example1.png’, ‘PNG’)
+gdd.write(‘example1.pdf’, ‘PDF’)
